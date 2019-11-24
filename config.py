8afe0f38dfead306b483bfe77c6514055fa77f92
leon.schiller <leon.schiller@student.hpi.uni-potsdam.de>
2019-11-24 22:59:15 +0100

    resized example images

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 config = {
-    'attribute': 'Male',
-    'parameter_range': (-2, 2.1, 1), # (min, max, step)
-    'image_filename': './images/000055.jpg',
+    'attribute': 'Smiling',
+    'parameter_range': (-0, 4.1, 1), # (min, max, step)
+    'image_filename': './images/me3.jpg',
     'sample_size': 5
 }
